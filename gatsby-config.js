d051f69d323b82a838c0547c73f50be73ab46919
Oguz Gelal <oguz.gelal@vub.ac.be>
2018-02-17 21:40:05 +0100

    compatibility stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,23 @@
+const pixrem = require('pixrem');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
   siteMetadata: {
     title: 'Oguz Gelal | Personal website',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'
+    {
+      resolve: `gatsby-plugin-postcss-sass`,
+      options: {
+        postCssPlugins: [
+          pixrem(),
+          autoprefixer({
+            browsers: ['last 5 versions']
+          })
+        ],
+        precision: 8
+      }
+    },
   ],
 };
